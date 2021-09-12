3d50d5ef1014bd2d45985b0e84b5d1df5a4f5194
jjunest <jjunest@gmail.com>
2021-09-12 11:38:21 +0900

    test runserver

--- test_runserver.py
+++ test_runserver.py
@@ -1,4 +1,8 @@
 # Press the green button in the gutter to run the script.
+from time import sleep
+
 if __name__ == '__main__':
-    for i in range(1, 5, 1):
+    print("this is test_runserver start() ")
+    for i in range(1, 1000, 1):
+        sleep(1)
         print("this is test_runserver.py")
