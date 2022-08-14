3665195e75f97d21320576bb02057df66ef3e81f
jjunest <jjunest@gmail.com>
2022-08-14 22:27:48 +0900

    stocklist

--- getinfo_stocklist.py
+++ getinfo_stocklist.py
@@ -138,10 +138,8 @@ def insert_info_into_db(stock_list_info_dataframe) :
         # stock_list_info_dataframe['vesting_type'] = stock_list_info_dataframe['vesting_type'].astype(str)
         # stock_list_info_dataframe['vesting_type_detail'] = stock_list_info_dataframe['vesting_type_detail'].astype(str)
         stock_list_info_dataframe['stock_name'] = stock_list_info_dataframe['stock_name'].astype(str)
-
-
         stock_list_info_tolist = stock_list_info_dataframe.values.tolist()
-        print("this is stock_list_info_tolist\n",stock_list_info_tolist)
+        print("this is stock_list_info_tolist")
         # 운영서버용 코드
         sqliteconnection = sqlite3.connect("/home/TheaterWin/db.sqlite3")
         # 개발로컬PC용 코드
