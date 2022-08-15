5f148a9b75469be791c5325e36559c226eeba162
jjunest <jjunest@gmail.com>
2022-08-15 14:44:59 +0900

    commit

--- test_apiDBtest.py
+++ test_apiDBtest.py
@@ -1,12 +1,18 @@
 import sqlite3
 import django
 
-
+# 개발환경 = local, 운영환경 = real
+# setting = 'real'
+setting = 'local'
 
 if __name__ == '__main__':
     print("this is apiDBtest")
     testtext = 'testtext'
-    conn = sqlite3.connect('C:\\Users\\jjune\\djangogirls\\TheaterWin\\db.sqlite3')
+    if setting in 'real':
+        # 운영서버용 코드
+        conn = sqlite3.connect("/home/TheaterWin/db.sqlite3")
+    if setting in 'local':
+        conn = sqlite3.connect('C:\\Users\\jjune\\djangogirls\\TheaterWin\\db.sqlite3')
     cur = conn.cursor()
     query = "INSERT INTO TheaterWinBook_FullvestingApi (fullvesting_text) VALUES (?)"
     cur.execute(query, (testtext,))
